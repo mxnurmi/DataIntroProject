055d4e5937f938e8349fdbffe125e59266ea0da9
Miska <miska.nurmi@helsinki.fi>
2019-10-22 12:24:11 +0300

    CSV ladattu ja datan muokkaus aloitettu

--- DataIntro.py
+++ DataIntro.py
@@ -9,4 +9,12 @@ Created on Mon Oct 21 13:23:31 2019
 import numpy as np
 import pandas as pd
 
+df = pd.read_csv('show-list.csv', delimiter = ';', encoding='cp1252')
+df['Show Time'] = df[['Show Time']].fillna(method='ffill')
 
+#TODO: Poista tyhjät sekä turhat palkit.Tärkeitä palkkeja ainakin alkuaika, admissions, 3D vai ei distributor, ja tyyppi
+
+#Exploration
+print(df.groupby('Event').count())
+#Huomioita: Sama elokuva voi esiintyä usealla eri nimellä. Lisäksi vaihtelevasti elokuvien täsmennyksiä.
+means = df.groupby('Event').mean() #pitää muuttaa tyypit oikein
