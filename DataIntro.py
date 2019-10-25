f020a3ee3520547f47c7ab237cd47dc5633972fc
Miska <miska.nurmi@helsinki.fi>
2019-10-25 12:34:11 +0300

    Sää tiedot lisätty, Datakenttiä päivitetty.

--- DataIntro.py
+++ DataIntro.py
@@ -8,19 +8,41 @@ Created on Mon Oct 21 13:23:31 2019
 
 import numpy as np
 import pandas as pd
+import holidays 
 
 
-df = pd.read_csv('show-list.csv', delimiter = ';', encoding='cp1252', usecols=[0, 1, 3, 4, 9, 10, 11, 12, 14, 18, 21])
+df = pd.read_csv('show-list.csv', delimiter = ';', encoding='cp1252', 
+                 usecols=[0, 1, 3, 4, 9, 10, 11, 12, 14, 18, 21])
+
+df.rename(columns={'Unnamed: 1': 'Time'}, inplace=True)
+
+weather = pd.read_csv('weather.csv', delimiter = ',')
 
 #
 df['Show Time'] = df['Show Time'].str.replace('Accounting Date: ', '')
 df['Show Time'] = df[['Show Time']].fillna(method='ffill')
-df = df.dropna(subset=['Unnamed: 1'])
+#df = df.dropna(subset=['Unnamed: 1'])
+df = df.dropna(subset=['Event'])
 df['Show Time'] = df['Show Time'].str.strip()
-df['Show Time']=pd.to_datetime(df['Show Time'])
+df['Show Time'] = pd.to_datetime(df['Show Time'] + ' ' + df['Time'])
+
+df = df.astype({"Admissions": "int"})
+
+#Holidays
+fin_holidays = holidays.FI()
+holiday = []
+columns = list(df)
+
+for i in df['Show Time']: 
+    holiday.append(i in fin_holidays)
+    
+df.insert(2, "Holiday", holiday, True)
 
 
-#TODO: Poista tyhjät sekä turhat palkit.Tärkeitä palkkeja ainakin alkuaika, admissions, 3D vai ei distributor, ja tyyppi
+
+
+#TODO: aika vertilu joka kääntää jokaisen kellonajan ja päivämäärän lähimpään tuntiin ja tekee 
+#sen pohjalta vertauksen sen hetkisestä säästä
 
 #Exploration
 print(df.groupby('Event').count())
